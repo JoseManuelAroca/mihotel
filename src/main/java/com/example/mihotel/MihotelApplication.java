1837a734d7e088ba4929b1d9214c98118f02e8ca
eneascarneiro <79973955+eneascarneiro@users.noreply.github.com>
2025-05-07 19:03:40 +0200

    primera version

---
+++ src/main/java/com/example/mihotel/MihotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mihotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MihotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MihotelApplication.class, args);
+	}
+
+}
